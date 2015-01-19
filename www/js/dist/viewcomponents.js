519568bb7e51980ddb8b79b590c26335caf1524d
Dirceu Pereira Tiegs <dirceutiegs@gmail.com>
2015-01-19 20:46:13 -0200

    Adds first version of the details page

--- www/js/dist/viewcomponents.js
+++ www/js/dist/viewcomponents.js
@@ -1,10 +1,18 @@
 /** @jsx React.DOM */
 
 var Route = React.createClass({displayName: "Route",
+  showRouteDetails: function() {
+    var searchTerm = $('#searchTerm').val();
+    React.render(
+      React.createElement(DetailsPage, {routeId: this.props.routeId, routeName: this.props.children.toString(), searchTerm: searchTerm}),
+      document.getElementById('page-content')
+    );
+  },
+
   render: function() {
     var routeName = this.props.children.toString();
     return (
-      React.createElement("li", {className: "item item-icon-right"}, 
+      React.createElement("li", {className: "item item-icon-right", onClick: this.showRouteDetails}, 
         routeName, 
         React.createElement("i", {className: "icon ion-chevron-right"})
       )
@@ -14,7 +22,7 @@ var Route = React.createClass({displayName: "Route",
 
 var RouteList = React.createClass({displayName: "RouteList",
   getInitialState: function() {
-    return {data: [{"id":22,"shortName":"131","longName":"AGRONÔMICA VIA GAMA D'EÇA","lastModifiedDate":"2009-10-26T02:00:00+0000","agencyId":9},{"id":32,"shortName":"133","longName":"AGRONÔMICA VIA MAURO RAMOS","lastModifiedDate":"2012-07-23T03:00:00+0000","agencyId":9}]};
+    return {data: []};
   },
 
   componentDidMount: function() {
@@ -50,9 +58,127 @@ var RouteList = React.createClass({displayName: "RouteList",
   }
 });
 
+var StopList = React.createClass({displayName: "StopList",
+  render: function() {
+    var stops = this.props.data.map(function (stop) {
+      return (
+        React.createElement("li", {className: "item"}, 
+          stop.name
+        )
+      );
+    });
+    return (
+      React.createElement("div", {className: "stop-list"}, 
+        React.createElement("div", {className: "row"}, 
+          React.createElement("h2", null, "Stops")
+        ), 
+        React.createElement("div", {className: "row"}, 
+          React.createElement("ul", {className: "list"}, 
+            stops
+          )
+        )
+      )
+    );
+  }
+});
+
+var Timetable = React.createClass({displayName: "Timetable",
+  render: function() {
+    var departures = {'WEEKDAY': [], 'SATURDAY': [], 'SUNDAY': []};
+    this.props.data.forEach(function (departure) {
+       departures[departure.calendar].push(React.createElement("li", null, departure.time));
+    });
+    return (
+      React.createElement("div", {className: "timetable"}, 
+        React.createElement("div", {className: "row"}, 
+          React.createElement("h2", null, "Timetable")
+        ), 
+        React.createElement("div", {className: "row responsive-sm"}, 
+          React.createElement("div", {className: "col"}, 
+            React.createElement("h3", null, "Weekday"), 
+            departures.WEEKDAY
+          ), 
+          React.createElement("div", {className: "col"}, 
+            React.createElement("h3", null, "Saturday"), 
+            departures.SATURDAY
+          ), 
+          React.createElement("div", {className: "col"}, 
+            React.createElement("h3", null, "Sunday"), 
+            departures.SUNDAY
+          )
+        )
+      )
+    );
+  }
+});
+
+var IndexPage = React.createClass({displayName: "IndexPage",
+  render: function() {
+    var searchTerm = this.props.searchTerm || '';
+    return (
+      React.createElement("div", {id: "route-list"}, 
+        React.createElement("form", {id: "searchRoutesForm"}, 
+          React.createElement("div", {className: "bar bar-header bar-light item-input-inset"}, 
+            React.createElement("label", {className: "item-input-wrapper"}, 
+              React.createElement("i", {className: "icon ion-ios7-search placeholder-icon"}), 
+              React.createElement("input", {type: "search", placeholder: "Street name", id: "searchTerm", defaultValue: searchTerm})
+            ), 
+            React.createElement("button", {className: "button button-positive"}, 
+              "Search"
+            )
+          )
+        ), 
+        React.createElement(RouteList, null)
+      )
+    );
+  }
+});
+
+var DetailsPage = React.createClass({displayName: "DetailsPage",
+  getInitialState: function() {
+    return {stops: [], departures: []};
+  },
+
+  componentWillMount: function () {
+    var self = this,
+        routeId = this.props.routeId;
+
+    floripaByBusAPI.stopsByRouteId(routeId, function(data) {
+      self.setState({stops: data.rows});
+    });
+
+    floripaByBusAPI.departuresByRouteId(routeId, function(data) {
+      self.setState({departures: data.rows});
+    });
+  },
+
+  goBack: function() {
+    var searchTerm = this.props.searchTerm || '';
+    React.render(
+      React.createElement(IndexPage, {searchTerm: searchTerm}),
+      document.getElementById('page-content')
+    );
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", {id: "route-details"}, 
+        React.createElement("div", {className: "bar bar-header bar-light item-input-inset"}, 
+          React.createElement("a", {className: "button button-clear button-dark icon ion-chevron-left", onClick: this.goBack}, 
+            "Back"
+          ), 
+          React.createElement("h1", {className: "title"}, this.props.routeName)
+        ), 
+        React.createElement(StopList, {data: this.state.stops}), 
+        React.createElement(Timetable, {data: this.state.departures})
+      )
+    );
+  }
+});
+
 $(function() {
   React.render(
-    React.createElement(RouteList, null),
-    document.getElementById('route-list')
+    React.createElement(IndexPage, null),
+    document.getElementById('page-content')
   );
 });
--- www/js/src/viewcomponents.js
+++ www/js/src/viewcomponents.js
@@ -1,10 +1,18 @@
 /** @jsx React.DOM */
 
 var Route = React.createClass({
+  showRouteDetails: function() {
+    var searchTerm = $('#searchTerm').val();
+    React.render(
+      <DetailsPage routeId={this.props.routeId} routeName={this.props.children.toString()} searchTerm={searchTerm} />,
+      document.getElementById('page-content')
+    );
+  },
+
   render: function() {
     var routeName = this.props.children.toString();
     return (
-      <li className="item item-icon-right">
+      <li className="item item-icon-right" onClick={this.showRouteDetails}>
         {routeName}
         <i className="icon ion-chevron-right"></i>
       </li>
@@ -14,7 +22,7 @@ var Route = React.createClass({
 
 var RouteList = React.createClass({
   getInitialState: function() {
-    return {data: [{"id":22,"shortName":"131","longName":"AGRONÔMICA VIA GAMA D'EÇA","lastModifiedDate":"2009-10-26T02:00:00+0000","agencyId":9},{"id":32,"shortName":"133","longName":"AGRONÔMICA VIA MAURO RAMOS","lastModifiedDate":"2012-07-23T03:00:00+0000","agencyId":9}]};
+    return {data: []};
   },
 
   componentDidMount: function() {
@@ -50,9 +58,127 @@ var RouteList = React.createClass({
   }
 });
 
+var StopList = React.createClass({
+  render: function() {
+    var stops = this.props.data.map(function (stop) {
+      return (
+        <li className="item">
+          {stop.name}
+        </li>
+      );
+    });
+    return (
+      <div className="stop-list">
+        <div className="row">
+          <h2>Stops</h2>
+        </div>
+        <div className="row">
+          <ul className="list">
+            {stops}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+var Timetable = React.createClass({
+  render: function() {
+    var departures = {'WEEKDAY': [], 'SATURDAY': [], 'SUNDAY': []};
+    this.props.data.forEach(function (departure) {
+       departures[departure.calendar].push(<li>{departure.time}</li>);
+    });
+    return (
+      <div className="timetable">
+        <div className="row">
+          <h2>Timetable</h2>
+        </div>
+        <div className="row responsive-sm">
+          <div className="col">
+            <h3>Weekday</h3>
+            {departures.WEEKDAY}
+          </div>
+          <div className="col">
+            <h3>Saturday</h3>
+            {departures.SATURDAY}
+          </div>
+          <div className="col">
+            <h3>Sunday</h3>
+            {departures.SUNDAY}
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var IndexPage = React.createClass({
+  render: function() {
+    var searchTerm = this.props.searchTerm || '';
+    return (
+      <div id="route-list">
+        <form id='searchRoutesForm'>
+          <div className="bar bar-header bar-light item-input-inset">
+            <label className="item-input-wrapper">
+              <i className="icon ion-ios7-search placeholder-icon"></i>
+              <input type="search" placeholder="Street name" id='searchTerm' defaultValue={searchTerm} />
+            </label>
+            <button className="button button-positive">
+              Search
+            </button>
+          </div>
+        </form>
+        <RouteList />
+      </div>
+    );
+  }
+});
+
+var DetailsPage = React.createClass({
+  getInitialState: function() {
+    return {stops: [], departures: []};
+  },
+
+  componentWillMount: function () {
+    var self = this,
+        routeId = this.props.routeId;
+
+    floripaByBusAPI.stopsByRouteId(routeId, function(data) {
+      self.setState({stops: data.rows});
+    });
+
+    floripaByBusAPI.departuresByRouteId(routeId, function(data) {
+      self.setState({departures: data.rows});
+    });
+  },
+
+  goBack: function() {
+    var searchTerm = this.props.searchTerm || '';
+    React.render(
+      <IndexPage searchTerm={searchTerm} />,
+      document.getElementById('page-content')
+    );
+  },
+
+  render: function() {
+    return (
+      <div id="route-details">
+        <div className="bar bar-header bar-light item-input-inset">
+          <a className="button button-clear button-dark icon ion-chevron-left" onClick={this.goBack}>
+            Back
+          </a>
+          <h1 className="title">{this.props.routeName}</h1>
+        </div>
+        <StopList data={this.state.stops} />
+        <Timetable data={this.state.departures} />
+      </div>
+    );
+  }
+});
+
 $(function() {
   React.render(
-    <RouteList />,
-    document.getElementById('route-list')
+    <IndexPage />,
+    document.getElementById('page-content')
   );
 });
